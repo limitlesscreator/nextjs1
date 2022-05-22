248db2c03bcf84a7b868e5873952a908fc553468
Vladislav V <0428vlad0428@mail.ru>
2022-05-22 19:11:41 +0300

    start project next.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css';
 import {AppProps} from "next/dist/shared/lib/router/router";
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+    return <>
+        <Head>
+            <title>MyTop</title>
+            <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <Component {...pageProps} />;
+    </>
 }
 
 export default MyApp;
